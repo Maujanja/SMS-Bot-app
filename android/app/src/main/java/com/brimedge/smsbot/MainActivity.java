7f7b692bd083ec5e5e9e071da64e89fe3ed753fd
Maujanja <omarysimba2010@gmail.com>
2026-07-10 13:20:41 +0300

    Add MainActivity.java - Main application activity

---
+++ android/app/src/main/java/com/brimedge/smsbot/MainActivity.java
@@ -0,0 +1,16 @@
+package com.brimedge.smsbot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("SMS Bot Running");
+    }
+}
